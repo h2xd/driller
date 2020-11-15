6713ca15cf0c724b0302cb16d29748d22fd3304b
Andreas Schujkow <andreas.schujkow@sumcumo.com>
2020-11-15 14:33:53 +0100

    feat(home): add vue 3 tests

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    {{ dima }}
+    {{ counter }}
+    {{ cube }}
   </div>
+  <button v-if="counter < 5" @click="countUp">+</button>
+  <button v-else @click="countDown">-</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent, ref, computed } from "vue";
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld
+  setup() {
+    const counter = ref(2);
+    const dima = ref("Human");
+
+    const countUp = () => (counter.value += 1);
+    const countDown = () => (counter.value -= 1);
+    const cube = computed(() => counter.value ** 2);
+
+    return {
+      counter,
+      dima,
+      countUp,
+      countDown,
+      cube
+    };
   }
 });
 </script>
