6520ad3599b4a6e92670ffe24f95ca32cbb4e8ff
C4mper0k <67705751+C4mper0k@users.noreply.github.com>
2020-11-17 19:28:43 +0700

    feat(bmr): add calculation for bmr (#2)
    added male algorithm

---
+++ src/components/Input.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label :for="name">{{ label }}</label>
+    <input
+      type="number"
+      min="1"
+      v-bind="$attrs"
+      v-model="$attrs.modelValue"
+      :name="name"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  inheritAttrs: false,
+  props: {
+    name: String,
+    label: String,
+    value: Object
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+label {
+  display: block;
+  text-align: middle;
+}
+</style>
---
+++ src/views/Settings.vue
@@ -0,0 +1,33 @@
+<template>
+  <Input label="Age" name="age" v-model="age" />
+  <Input label="Height" name="height" v-model="height" />
+  <Input label="Weight" name="weight" v-model="weight" />
+  bmr:{{ bmr }}
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from "vue";
+import Input from "@/components/Input.vue";
+
+export default defineComponent({
+  components: {
+    Input
+  },
+  setup() {
+    const age = ref(1);
+    const height = ref(1);
+    const weight = ref(1);
+
+    const bmr = computed(
+      () => 10 * weight.value + 6.25 * height.value - 5 * age.value + 5
+    );
+
+    return {
+      age,
+      height,
+      weight,
+      bmr
+    };
+  }
+});
+</script>
