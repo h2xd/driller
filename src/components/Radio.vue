0556fb24d4ac9a2bd8b81113a46304729c3db66e
Dmitry K <korkin777@gmail.com>
2020-11-21 22:43:28 +0700

    chore(radio): add radio example

---
+++ src/components/Radio.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-for="(element, index) in group" :key="index">
+    <label :for="name">{{ element.label }}</label>
+    <input
+      type="radio"
+      :name="name"
+      :value="element.value"
+      v-model="element.checked"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+type RadioElement = {
+  name: string;
+  label: string;
+  value: string | number;
+  checked: boolean;
+};
+
+export default defineComponent({
+  inheritAttrs: false,
+  props: {
+    group: { type: Array as PropType<RadioElement[]>, required: true },
+    name: String
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+label {
+  display: block;
+  text-align: middle;
+}
+</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,21 +2,27 @@
   <Input label="Age" name="age" v-model="age" />
   <Input label="Height" name="height" v-model="height" />
   <Input label="Weight" name="weight" v-model="weight" />
-  bmr:{{ bmr }}
+  <Radio :group="genderGroup" name="gender" /> bmr:{{ bmr }}
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, computed } from "vue";
 import Input from "@/components/Input.vue";
+import Radio from "@/components/Radio.vue";
 
 export default defineComponent({
   components: {
-    Input
+    Input,
+    Radio
   },
   setup() {
     const age = ref(1);
     const height = ref(1);
     const weight = ref(1);
+    const genderGroup = [
+      { label: "Male", checked: true, value: "male" },
+      { label: "Female", checked: false, value: "female" }
+    ];
 
     const bmr = computed(
       () => 10 * weight.value + 6.25 * height.value - 5 * age.value + 5
@@ -26,6 +32,7 @@ export default defineComponent({
       age,
       height,
       weight,
+      genderGroup,
       bmr
     };
   }
