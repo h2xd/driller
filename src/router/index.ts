542c6e9971719325f4883b032a070c4b3fc57f5c
Andreas Schujkow <andreas.schujkow@sumcumo.com>
2020-12-26 14:13:48 +0100

    feat(router): use aliases

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,14 +13,13 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "about" */ "@/views/About.vue")
   },
   {
     path: "/settings",
     name: "Settings",
     component: () =>
-      import(/* webpackChunkName: "settings" */ "../views/Settings.vue")
+      import(/* webpackChunkName: "settings" */ "@/views/Settings.vue")
   }
 ];
 
